d640dd2ec35e046fbd8cba60c4ba2f5aa76fbe01
tporter13 <teaspoon13@verizon.net>
2015-09-05 14:49:51 -0400

    trying to get latest changes into master branch

--- ParseRecipes.py
+++ ParseRecipes.py
@@ -24,14 +24,16 @@ htmlfile = open(sys.argv[2],"w")
  
 # initialize rownum variable
 rownum = 0
+prevTag = ''
 
-# Style Header 
+# Style Header
 htmlfile.write('''<ul><style type="text/css"> 
-                        h1 {font-weight:bold;font-size:20  px} 
+                        h1 {f   ont-weight:bold;font-size:20  px} 
                         li {font-weight:normal;font-size:14px;list-style-type: none;} 
-                        li.recipe-heading {font-weight:bold;font-size:16px;padding-left:0px;padding-top:5px;list-style-type: none;} 
-                        li.recipe-instructions {font-weight:normal;font-size:14px;padding-left:5px;list-style-type: none;}
-                        li.recipe-tag {font-weight:normal;font-style:italic;font-size:12px;padding-left:5px;padding-top:3px;}
+                        li.recipe-heading {font-weight:bold;font-size:14px;padding-left:5px;padding-top:10px;list-style-type: none;} 
+                        li.recipe-instructions {font-weight:normal;font-size:12px;padding-left:10px;list-style-type: none;}
+                        li.recipe-tag {font-weight:normal;font-style:italic;font-size:11px;padding-left:5px;padding-top:3px;}
+                        li.recipe-tag-heading{font-weight:bold;font-size:16px;padding-left:0px;padding-top:20px;padding-bottom:5px;list-style-type: none;}
                     </style>''')
 
 # write <table> tag
@@ -39,16 +41,22 @@ htmlfile.write('<ul>')
 
 # generate table contents
 for row in sortedreader: # Read a single row from the CSV file
-    htmlfile.write('<li class="recipe-heading">' + row[0] + '</li>')
+    # if first row or tag has changed, write tag as heading
+    if rownum == 0:
+        htmlfile.write('<li class="recipe-tag-heading">' + row[2] + '</li>')
+    elif row[2] != prevTag:
+        htmlfile.write('<li class="recipe-tag-heading">' + row[2] + '</li>')
+    
+    htmlfile.write('<li class="recipe-heading">' + row[0].replace('\n', '<br />') + '</li>')
     htmlfile.write('<li class="recipe-instructions">' + row[1].replace('\n', '<br />') + '</li>')
-    htmlfile.write('<li class="recipe-tag">' + row[2] + '</li>')
     # write header row. assumes first row in csv contains header
     # for column in row:
         # htmlfile.write('<li>' + column + '</li>')
      
+    prevTag = row[2]
     #increment row count    
     rownum += 1
-# write </table> tag
+
 htmlfile.write('</ul>')
  
 # print results to shell
